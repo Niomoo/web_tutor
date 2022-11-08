e9aa659481ccdb19e7c18f1cb8cbffaea0634757
Niomoo <jennyliu.lyh@gmail.com>
2022-11-08 21:15:08 +0800

    [Week34]Modify guess number display

--- Week33/w33_practice.js
+++ Week33/w33_practice.js
@@ -11,17 +11,18 @@ function guess() {
     let hint = document.getElementById("hint");
     count++;
     if(number == num) {
-        hint.innerText = "猜對了！你總共猜了 " + count + "次！";
+        hint.innerText = "猜對了！你總共猜了 " + count + " 次！";
         hint.style.color = 'blue';
+        count = 0;
     } else {
         hint.innerText = '猜錯了！';
         hint.style.color = 'red';
         if(number < num) {
             min = number;
-            hint.innerText = hint.innerText + '答案在 ' + min + ' ~ ' + max + '之間！';
+            hint.innerText = hint.innerText + '答案在 ' + min + ' ~ ' + max + ' 之間！';
         } else {            
             max = number;
-            hint.innerText = hint.innerText + "答案在 " + min + " ~ " + max + "之間！";
+            hint.innerText = hint.innerText + "答案在 " + min + " ~ " + max + " 之間！";
         }
     }
     document.getElementById("count").innerText = "猜測次數：" + count;
